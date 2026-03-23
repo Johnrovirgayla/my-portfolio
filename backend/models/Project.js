ccebf78d73dbf7e47996e4357b40bf84141ee76f
Johnrovirgayla <joca.gayla.coc@phinmaed.com>
2026-03-23 16:30:00 +0800

    Create Project.js model with MongoDB schema for projects

---
+++ backend/models/Project.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    technologies: { type: [String], required: true },
+    links: { type: [String], required: true },
+    category: { type: String, required: true },
+},{ timestamps: true });
+
+module.exports = mongoose.model('Project', projectSchema);
