77da2cb71e2edcd8bc6291f116bed4f468630968
Johnrovirgayla <joca.gayla.coc@phinmaed.com>
2026-03-23 18:57:41 +0800

    Add navigation component file includes/navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,14 @@
+<?php
+// Navigation component
+
+function renderNavbar() {
+    echo '<nav>';
+    echo '    <ul>';
+    echo '        <li><a href="/">Home</a></li>';
+    echo '        <li><a href="/about">About</a></li>';
+    echo '        <li><a href="/projects">Projects</a></li>';
+    echo '        <li><a href="/contact">Contact</a></li>';
+    echo '    </ul>';
+    echo '</nav>';
+}
+?>
